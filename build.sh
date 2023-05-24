48fe2af1293917c0b986e34c336aba1a435cae76
Matheo <matheo.martin@epitech.eu>
2023-05-24 20:03:36 +0200

    fix compilation

--- build.sh
+++ build.sh
@@ -5,15 +5,14 @@ if [ ! -d "build" ]; then
     mkdir build
 fi
 if [ ! -f "./git/hooks/pre-commit" ]; then
-    touch ./git/hooks/pre-commit
-    echo "#! /bin/bash" >> ./git/hooks/pre-commit
-    echo "sh clean.sh" >> ./git/hooks/pre-commit
-    chmod +x ./git/hooks/pre-commit
+    touch .git/hooks/pre-commit
+    echo "#! /bin/bash" >> .git/hooks/pre-commit
+    echo "sh clean.sh" >> .git/hooks/pre-commit
+    chmod +x .git/hooks/pre-commit
 fi
 cd build
 cmake ..
 make -j16
 cp zappy_gui ../
 cp zappy_server ../
-cp zappy_ia ../
 cd ..
