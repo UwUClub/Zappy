ca34e2a9782268beaaee65a882fa8f8eb77c71da
Clang Robot <robot@example.com>
2023-05-25 08:20:26 +0000

    :art: Committing clang-format changes

--- gui/src/cli/ParserData.cpp
+++ gui/src/cli/ParserData.cpp
@@ -1,39 +1,31 @@
 #include "ParserData.hpp"
 
 namespace Zappy::GUI {
-    ParserData::ParserData(std::string address, int port, std::string teamName) {
-        _address = address;
-        _port = port;
-        _teamName = teamName;
-    }
+ParserData::ParserData(std::string address, int port, std::string teamName)
+{
+    _address = address;
+    _port = port;
+    _teamName = teamName;
+}
 
-    void ParserData::parseData(int argc, char **argv) {
-        std::string portStr;
+void ParserData::parseData(int argc, char **argv)
+{
+    std::string portStr;
 
-        if (argc < 3) {
-            throw ParserException("Not enough arguments");
-        }
-        if (argv[1] == NULL || argv[2] == NULL) {
-            throw ParserException("Invalid arguments given");
-        }
-        portStr = argv[2];
-        _port = std::stoi(portStr);
-        if (argv[3] != NULL && argv[4] != NULL) {
-            _teamName = argv[3];
-        } else {
-            _teamName = "localhost";
-        }
-    }
-
-    std::string ParserData::getAddress() const {
-        return _address;
-    }
-
-    int ParserData::getPort() const {
-        return _port;
-    }
-
-    std::string ParserData::getTeamName() const {
-        return _teamName;
+    if (argc < 3) { throw ParserException("Not enough arguments"); }
+    if (argv[1] == NULL || argv[2] == NULL) { throw ParserException("Invalid arguments given"); }
+    portStr = argv[2];
+    _port = std::stoi(portStr);
+    if (argv[3] != NULL && argv[4] != NULL) {
+        _teamName = argv[3];
+    } else {
+        _teamName = "localhost";
     }
 }
+
+std::string ParserData::getAddress() const { return _address; }
+
+int ParserData::getPort() const { return _port; }
+
+std::string ParserData::getTeamName() const { return _teamName; }
+}// namespace Zappy::GUI
--- gui/src/cli/ParserData.hpp
+++ gui/src/cli/ParserData.hpp
@@ -1,32 +1,34 @@
 #include <iostream>
 
 namespace Zappy::GUI {
-    class ParserData {
-        public :
-            ParserData(std::string address, int port, std::string teamName);
-            ~ParserData() = default;
+class ParserData
+{
+  public:
+    ParserData(std::string address, int port, std::string teamName);
+    ~ParserData() = default;
 
-            void parseData(int argc, char **argv);
+    void parseData(int argc, char **argv);
 
-            std::string getAddress() const;
-            int getPort() const;
-            std::string getTeamName() const;
+    std::string getAddress() const;
+    int getPort() const;
+    std::string getTeamName() const;
 
-            class ParserException : public std::exception
-            {
-            public:
-                explicit ParserException(const std::string &aMessage) : _message(aMessage) {}
+    class ParserException : public std::exception
+    {
+      public:
+        explicit ParserException(const std::string &aMessage) : _message(aMessage) {}
 
-                ~ParserException() override = default;
+        ~ParserException() override = default;
 
-                [[nodiscard]] const char *what() const noexcept override { return _message.c_str(); }
+        [[nodiscard]] const char *what() const noexcept override { return _message.c_str(); }
 
-            private:
-                std::string _message;
-            };
-        private :
-            std::string _address;
-            int _port;
-            std::string _teamName;
+      private:
+        std::string _message;
     };
-}
+
+  private:
+    std::string _address;
+    int _port;
+    std::string _teamName;
+};
+}// namespace Zappy::GUI
