1dfbfb039e0d872c460d1a1212ac9a2187c61e4d
hp <christophe.chhor@epitech.eu>
2023-05-25 10:14:52 +0200

    Add parser in ParserData class, init a constructor

--- gui/src/cli/ParserData.hpp
+++ gui/src/cli/ParserData.hpp
@@ -3,7 +3,30 @@
 namespace Zappy::GUI {
     class ParserData {
         public :
-            int port;
-            std::string teamName;
+            ParserData(std::string address, int port, std::string teamName);
+            ~ParserData() = default;
+
+            void parseData(int argc, char **argv);
+
+            std::string getAddress() const;
+            int getPort() const;
+            std::string getTeamName() const;
+
+            class ParserException : public std::exception
+            {
+            public:
+                explicit ParserException(const std::string &aMessage) : _message(aMessage) {}
+
+                ~ParserException() override = default;
+
+                [[nodiscard]] const char *what() const noexcept override { return _message.c_str(); }
+
+            private:
+                std::string _message;
+            };
+        private :
+            std::string _address;
+            int _port;
+            std::string _teamName;
     };
 }
