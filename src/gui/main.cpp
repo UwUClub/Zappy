4dad2116eb243bf076fcf050d8eb1f4ee0325114
Clang Robot <robot@example.com>
2023-05-25 09:31:02 +0000

    :art: Committing clang-format changes

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -6,8 +6,8 @@
 
 int main(int argc, char **argv)
 {
-  (void)argc;
-  (void)argv;
-  std::cout << "Hello, world!" << std::endl;
-  return 0;
+    (void) argc;
+    (void) argv;
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
 }
