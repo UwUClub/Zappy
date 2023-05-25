3533ea62899bc96cdf9840f979aea3b54a045af1
hp <christophe.chhor@epitech.eu>
2023-05-25 11:38:19 +0200

    Add the norm of variable

--- gui/src/cli/ParserData.cpp
+++ gui/src/cli/ParserData.cpp
@@ -1,40 +1,38 @@
 #include "ParserData.hpp"
 
 namespace Zappy::GUI {
-ParserData::ParserData(std::string address, int port, std::string teamName)
+ParserData::ParserData(std::string aAddress, int aPort, std::string aMachineName)
 {
-    _address = address;
-    _port = port;
-    _machineName = teamName;
+    _address = aAddress;
+    _port = aPort;
+    _machineName = aMachineName;
 }
 
 void ParserData::parseData(int argc, char **argv)
 {
-    std::string firstOpt;
-    std::string secondOpt;
+    std::string myFirstOpt;
+    std::string mySecondOpt;
 
     if (argc < 3) { throw ParserException("Not enough arguments"); }
     if (argv[1] == NULL || argv[2] == NULL) { throw ParserException("Invalid arguments given"); }
-    firstOpt = argv[1];
-    if (firstOpt.compare("-p") != 0 && firstOpt.compare("-h") != 0) {
+    myFirstOpt = argv[1];
+    if (myFirstOpt.compare("-p") != 0 && myFirstOpt.compare("-h") != 0) {
         throw ParserException("Invalid arguments given");
     }
-    if (firstOpt.compare("-p") == 0) {
-        firstOpt = argv[2];
-        _port = std::stoi(firstOpt);
-    } else if (firstOpt.compare("-h") == 0) {
-        firstOpt = argv[2];
-        _machineName = firstOpt;
+    if (myFirstOpt.compare("-p") == 0) {
+        _port = std::stoi(argv[2]);
+    } else if (myFirstOpt.compare("-h") == 0) {
+        _machineName = argv[2];
     }
     if (argc > 3) {
         if (argv[3] != NULL && argv[4] != NULL) {
-            secondOpt = argv[3];
-            if (secondOpt.compare("-h") != 0 && secondOpt.compare("-p") != 0) {
+            mySecondOpt = argv[3];
+            if (mySecondOpt.compare("-h") != 0 && mySecondOpt.compare("-p") != 0) {
                 throw ParserException("Invalid arguments given");
             }
-            if (secondOpt.compare("-h") == 0) {
+            if (mySecondOpt.compare("-h") == 0) {
                 _machineName = argv[4];
-            } else if (secondOpt.compare("-p") == 0) {
+            } else if (mySecondOpt.compare("-p") == 0) {
                 _port = std::stoi(argv[4]);
             }
         }
