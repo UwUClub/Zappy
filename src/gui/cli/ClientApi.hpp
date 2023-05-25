31866f724664466ff2a8861afad4635bfc8f6359
Clang Robot <robot@example.com>
2023-05-25 14:57:24 +0000

    :art: Committing clang-format changes

--- src/gui/cli/ClientApi.hpp
+++ src/gui/cli/ClientApi.hpp
@@ -11,27 +11,27 @@ namespace Zappy::GUI {
             ~ClientApi();
 
             /**
-            * @brief Connect to the server
-            */
+             * @brief Connect to the server
+             */
             void joinGame();
             /**
-            * @brief Disconnect from the server
-            */
+             * @brief Disconnect from the server
+             */
             void disconnect();
             /**
-            * @brief Get connection status: 0 if not connected, 1 if connected
-            * @return Status
-            */
+             * @brief Get connection status: 0 if not connected, 1 if connected
+             * @return Status
+             */
             int getConnectStatus() const;
             /**
-            * @brief Get the file descriptor used to write to the server
-            * @return File descriptor
-            */
+             * @brief Get the file descriptor used to write to the server
+             * @return File descriptor
+             */
             int getServerFd() const;
             /**
-            * @brief Execute read / write flow with the server
-            * @return Update status
-            */
+             * @brief Execute read / write flow with the server
+             * @return Update status
+             */
             int update();
 
             class ClientException : public std::exception
@@ -53,26 +53,26 @@ namespace Zappy::GUI {
 
         private:
             /**
-            * @brief Get a sockaddr_in structure
-            * @param aAddress Address
-            * @param aPort Port
-            * @return sockaddr_in structure
-            */
+             * @brief Get a sockaddr_in structure
+             * @param aAddress Address
+             * @param aPort Port
+             * @return sockaddr_in structure
+             */
             struct sockaddr_in getSockaddr(in_addr_t aAddress, unsigned int aPort);
             /**
-            * @brief Read from the server and append the result to the read buffer
-            */
+             * @brief Read from the server and append the result to the read buffer
+             */
             void readFromServer();
             /**
-            * @brief Write to the server and remove the written data from the write buffer
-            */
+             * @brief Write to the server and remove the written data from the write buffer
+             */
             void writeToServer();
             /**
-            * @brief Concatenate two char *
-            * @param aStr1 First string
-            * @param aStr2 Second string
-            * @return Concatenated string
-            */
+             * @brief Concatenate two char *
+             * @param aStr1 First string
+             * @param aStr2 Second string
+             * @return Concatenated string
+             */
             char *concatStr(char *aStr1, const char *aStr2);
 
             char *_address;
