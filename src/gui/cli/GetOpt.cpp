85d1c1fbadc04cd1ec0734c5b821b5379a72e7f9
test <christophe.chhor@epitech.eu>
2023-05-26 11:10:05 +0200

    Add encapsulation of getOpt C function

---
+++ src/gui/cli/GetOpt.cpp
@@ -0,0 +1,26 @@
+#include "GetOpt.hpp"
+
+namespace Zappy::GUI {
+    GetOpt::GetOpt(int argc, char **argv) {
+        int opt;
+
+        while ((opt = getopt(argc, argv, "p:h:")) != -1) {
+            switch (opt) {
+                case 'p':
+                    Options["port"] = optarg;
+                    break;
+                case 'h':
+                    Options["machine"] = optarg;
+                    break;
+                default:
+                    throw GetOptException("Invalid argument");
+            }
+        }
+    }
+
+    GetOpt::~GetOpt() {}
+
+    std::map<std::string, std::string> GetOpt::getOptions() const {
+        return Options;
+    }
+}
---
+++ src/gui/cli/GetOpt.hpp
@@ -0,0 +1,44 @@
+#include <getopt.h>
+#include <string>
+#include <map>
+
+namespace Zappy::GUI {
+    class GetOpt {
+        public:
+            GetOpt(int argc, char **argv);
+            ~GetOpt();
+
+            /**
+             * @brief geting the options
+             * @param void
+             * @return options
+            */
+            std::map<std::string, std::string> getOptions() const;
+            
+            /**
+             * @brief exception class for getOpt
+             */
+            class GetOptException : public std::exception {
+                public:
+                    /**
+                     * @brief GetOptException constructor
+                     * @param aMessage
+                     */
+                    GetOptException(const std::string &aMessage) : Message(aMessage) {}
+                    ~GetOptException() = default;
+
+                    /**
+                     * @brief return the message of the exception
+                     * @return message
+                     */
+                    [[nodiscard]] const char *what() const noexcept override {
+                        return Message.c_str();
+                    }
+                private:
+                    std::string Message;
+            };
+
+        private:
+            std::map<std::string, std::string> Options;
+    };
+}
