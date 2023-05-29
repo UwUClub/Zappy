b8f2fa011bd19320ea51ebf1b2f5580d00fd3b0b
Clang Robot <robot@example.com>
2023-05-29 22:17:08 +0000

    :art: Committing clang-format changes

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -2,18 +2,18 @@
 // Created by beafowl on 22/05/23.
 //
 
+#include <OGRE/Bites/OgreApplicationContext.h>
+#include <OGRE/Ogre.h>
+#include <OGRE/OgreSceneManager.h>
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
-#include <OGRE/Ogre.h>
-#include <OGRE/Bites/OgreApplicationContext.h>
-#include <OGRE/OgreSceneManager.h>
 #include "ClientApi.hpp"
 #include "ParserData.hpp"
 
 int main()
 {
-    //open a window with a title
+    // open a window with a title
     OgreBites::ApplicationContext ctx("Zappy");
     ctx.initApp();
 }
