920ddb7ed4b3ced93f341e2dce065fc5fd410bf3
Valentin Gegoux <geg.valentin@gmail.com>
2023-05-29 12:59:52 +0200

    Call bct for each tile when team is GRAPHIC

--- src/server/implementation/cmd/bct.c
+++ src/server/implementation/cmd/bct.c
@@ -16,6 +16,7 @@ static void concat_str_item(char **result, const int item_quantity)
     str_item_quantity = inttos(item_quantity);
     *result = concat_str(*result, " ");
     *result = concat_str(*result, str_item_quantity);
+    free(str_item_quantity);
 }
 
 static char *get_tile_at(data_t *data, const int x, const int y)
@@ -35,6 +36,18 @@ static char *get_tile_at(data_t *data, const int x, const int y)
     return result;
 }
 
+static int parse_args(data_t *data, char **args, int *x, int *y)
+{
+    if (!args || word_array_len(args) != 2 || !is_int(args[0]) ||
+    !is_int(args[1]))
+        return 84;
+    *x = atoi(args[0]);
+    *y = atoi(args[1]);
+    if (*x < 0 || *x >= data->map_width || *y < 0 || *y >= data->map_height)
+        return 84;
+    return 0;
+}
+
 int bct(data_t *data, char **args)
 {
     char *msg = NULL;
@@ -42,15 +55,13 @@ int bct(data_t *data, char **args)
     int x = 0;
     int y = 0;
 
-    if (!args || word_array_len(args) != 2 || !is_int(args[0]) ||
-    !is_int(args[1]))
+    if (parse_args(data, args, &x, &y) == 84)
         return 1;
-    x = atoi(args[0]);
-    y = atoi(args[1]);
-    if (x < 0 || x >= data->map_width || y < 0 || y >= data->map_height)
-        return 1;
-    msg = strdup("bct");
-    result = get_tile_at(data, atoi(args[0]), atoi(args[1]));
+    msg = strdup("bct ");
+    msg = concat_str(msg, args[0]);
+    msg = concat_str(msg, " ");
+    msg = concat_str(msg, args[1]);
+    result = get_tile_at(data, x, y);
     msg = concat_str(msg, result);
     free(result);
     send_to_client(data->clients, data->curr_cli_index, msg);
