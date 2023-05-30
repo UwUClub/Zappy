cfdeb314e337084a1c9ad016c4aa024e6e408cee
Clang Robot <robot@example.com>
2023-05-30 21:21:00 +0000

    :art: Committing clang-format changes

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -12,18 +12,18 @@
 #include "ParserData.hpp"
 
 int main(int argc, char **argv)
-    try {
-        Zappy::GUI::ParserData parserData("127.0.0.1", 0, "");
-        parserData.parseData(argc, argv);
-        Zappy::GUI::ClientApi myClientApi(parserData.getAddress(), parserData.getPort(), "GRAPHIC");
-        myClientApi.joinGame();
-        while (true) {
-            if (myClientApi.update() >= 1) {
-                break;
-            }
+try {
+    Zappy::GUI::ParserData parserData("127.0.0.1", 0, "");
+    parserData.parseData(argc, argv);
+    Zappy::GUI::ClientApi myClientApi(parserData.getAddress(), parserData.getPort(), "GRAPHIC");
+    myClientApi.joinGame();
+    while (true) {
+        if (myClientApi.update() >= 1) {
+            break;
         }
-    } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
     }
-    return 0;
+} catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+}
+return 0;
 }
