4183e93f28f63e233cde6d4cf1853d67f5bde752
Clang Robot <robot@example.com>
2023-05-31 14:31:25 +0000

    :art: Committing clang-format changes

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -5,28 +5,30 @@
 #include "ClientApi.hpp"
 #include "ParserData.hpp"
 
-class Test {
-public:
-    Test(std::string mes) : _mes(std::move(mes)) {}
+class Test
+{
+    public:
+        Test(std::string mes) : _mes(std::move(mes))
+        {}
 
-    std::string _mes;
+        std::string _mes;
 };
 
 int main(int argc, char **argv)
 {
-//    try {
-//        Zappy::GUI::ParserData parserData("127.0.0.1", 0, "");
-//        parserData.parseData(argc, argv);
-//        Zappy::GUI::ClientApi myClientApi(parserData.getAddress(), parserData.getPort(), "GRAPHIC");
-//        myClientApi.joinGame();
-//        while (true) {
-//            if (myClientApi.update() >= 1) {
-//                break;
-//            }
-//        }
-//    } catch (const std::exception &e) {
-//        std::cerr << e.what() << std::endl;
-//    }
+    //    try {
+    //        Zappy::GUI::ParserData parserData("127.0.0.1", 0, "");
+    //        parserData.parseData(argc, argv);
+    //        Zappy::GUI::ClientApi myClientApi(parserData.getAddress(), parserData.getPort(), "GRAPHIC");
+    //        myClientApi.joinGame();
+    //        while (true) {
+    //            if (myClientApi.update() >= 1) {
+    //                break;
+    //            }
+    //        }
+    //    } catch (const std::exception &e) {
+    //        std::cerr << e.what() << std::endl;
+    //    }
     std::string mes = "Hello World";
     Test test(mes);
     std::cout << mes << std::endl;
