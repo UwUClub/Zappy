63d8c4768ab185c937da920133dde24474aa8c8a
Baptiste Laran <baptiste.laran@epitech.eu>
2023-05-31 11:33:12 +0200

    Add Set object command

--- src/server/implementation/ia/set.c
+++ src/server/implementation/ia/set.c
@@ -2,38 +2,27 @@
 ** EPITECH PROJECT, 2023
 ** Zappy
 ** File description:
-** take
+** set
 */
 
 #include <stdio.h>
 #include "server_implementation.h"
 #include "utils.h"
 
-static int check_is_on_tile(data_t *data, int resource)
-{
-    int x = data->clients[data->curr_cli_index]->pos_x;
-    int y = data->clients[data->curr_cli_index]->pos_y;
-    int *tile = data->map_tiles[y][x];
-
-    if (tile[resource] > 0) {
-        data->map_tiles[y][x][resource] -= 1;
-        return 0;
-    }
-    return 1;
-}
-
-int take(data_t *data, char **args)
+int set(data_t *data, char **args)
 {
     const char *resource[7] = {"food", "linemate", "deraumere", "sibur", "mendiane",
     "phiras", "thystame"};
-    
+
     if (args == NULL || word_array_len(args) > 1) {
         return 1;
     }
     for (int i = 0; i < 7; i++) {
         if (strcmp(args[0], resource[i]) == 0
-            && check_is_on_tile(data, i) == 0) {
-            data->clients[data->curr_cli_index]->inventory[i] += 1;
+            && data->clients[data->curr_cli_index]->inventory[i] > 0) {
+            data->clients[data->curr_cli_index]->inventory[i] -= 1;
+            data->map_tiles[data->clients[data->curr_cli_index]->pos_y]
+            [data->clients[data->curr_cli_index]->pos_x][i] += 1;
             send_to_client(data->clients, data->curr_cli_index, "ok\n");
             return 0;
         }
