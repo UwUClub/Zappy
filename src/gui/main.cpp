01b09ced9db023a1cc219d6ff23ac5024a3fc1a6
Clang Robot <robot@example.com>
2023-05-31 14:39:49 +0000

    :art: Committing clang-format changes

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -5,7 +5,8 @@
 #include "ClientApi.hpp"
 #include "ParserData.hpp"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     try {
         Zappy::GUI::ParserData parserData("127.0.0.1", 0, "");
         parserData.parseData(argc, argv);
