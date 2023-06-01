4f10f1869856e28267dc43abd5a4249d439cc72d
Valentin Gegoux <geg.valentin@gmail.com>
2023-06-01 16:32:49 +0200

    Add timeout calculation

--- src/server/core/handle_clients.c
+++ src/server/core/handle_clients.c
@@ -8,6 +8,7 @@
 #include <errno.h>
 #include <signal.h>
 #include <string.h>
+#include <sys/time.h>
 #include <time.h>
 #include "implementation.h"
 
@@ -49,25 +50,49 @@ static void run_client_flow(data_t *data, const int cli_index,
     }
 }
 
+struct timeval *get_timeout(data_t *data)
+{
+    struct timeval *timeout = NULL;
+    long smallest_remaining = -1;
+
+    for (int i = 0; data->clients[i]; i++) {
+        if (data->clients[i]->fd >= 0 && data->clients[i]->player &&
+            data->clients[i]->player->pending_cmd_queue[0] &&
+            (smallest_remaining == -1 ||
+            data->clients[i]->player->pending_cmd_queue[0]->remaining <
+                smallest_remaining)) {
+                smallest_remaining =
+                    data->clients[i]->player->pending_cmd_queue[0]->remaining;
+        }
+    }
+    if (smallest_remaining == -1)
+        return NULL;
+    timeout = malloc(sizeof(struct timeval));
+    timeout->tv_sec = smallest_remaining;
+    timeout->tv_usec = 0;
+    return timeout;
+}
+
 int select_clients(struct sockaddr_in *addr, int server_fd, data_t *data)
 {
     fd_set read_fd_set;
     fd_set write_fd_set;
+    struct timeval *timeout = NULL;
 
+    timeout = get_timeout(data);
     get_fd_set(data->clients, &read_fd_set, &write_fd_set);
     FD_SET(server_fd, &read_fd_set);
     time(&data->last_select);
-    select(FD_SETSIZE, &read_fd_set, &write_fd_set, NULL, NULL);
+    select(FD_SETSIZE, &read_fd_set, &write_fd_set, NULL, timeout);
+    free(timeout);
+    // TODO calc remaining of all players
     // printf("waited: %f\n", time(NULL) - data->last_select);
-    if (!keep_running) {
+    if (!keep_running)
         return 1;
-    }
-    if (FD_ISSET(server_fd, &read_fd_set)) {
+    if (FD_ISSET(server_fd, &read_fd_set))
         welcome_selected_client((struct sockaddr *) addr, server_fd,
         &(data->clients));
-    }
-    for (int i = 0; data->clients[i]; i++) {
+    for (int i = 0; data->clients[i]; i++)
         run_client_flow(data, i, read_fd_set, write_fd_set);
-    }
     return 0;
 }
