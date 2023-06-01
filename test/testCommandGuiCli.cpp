fe53e242eac7f831a6c378b7726f4f7bb1ba2f63
Matheo <matheo.martin@epitech.eu>
2023-06-01 11:21:17 +0200

    Add Tests or tna

--- test/testCommandGuiCli.cpp
+++ test/testCommandGuiCli.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "../src/gui/cli/ClientApi.hpp"
 #include "../src/gui/cli/ParserData.hpp"
-#include "../src/gui/cli/ServerData.hpp"
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("testingBct", "[testingBct]")
@@ -38,4 +37,21 @@ TEST_CASE("testingMsz", "[testingMsz]")
     myClientApi.disconnect();
     REQUIRE(myClientApi.getServerData()._mapSize.first == 10);
     REQUIRE(myClientApi.getServerData()._mapSize.second == 10);
+}
+
+TEST_CASE("testingTna", "[testingTna]")
+{
+    Zappy::GUI::ParserData const parserData("127.0.0.1", 4242, "");
+    Zappy::GUI::ClientApi myClientApi(parserData.getAddress(), parserData.getPort(), "GRAPHIC");
+    myClientApi.joinGame();
+    while (true) {
+        if (myClientApi.update() >= 1) {
+            break;
+        }
+        if (!myClientApi.getServerData()._teamNames.empty()) {
+            break;
+        }
+    }
+    myClientApi.disconnect();
+    REQUIRE(!myClientApi.getServerData()._teamNames.empty());
 }
