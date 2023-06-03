edc6d2ec96fd7ec3e5cbe79bc2d637a2619f6e8f
hp <christophe.chhor@epitech.eu>
2023-06-03 16:19:42 +0200

    Update the status of the seg function as public function

--- src/server/implementation/event/seg.c
+++ src/server/implementation/event/seg.c
@@ -12,14 +12,13 @@
 static void print_seg_ctrl_c(data_t *data, char *msg)
 {
     for (int i = 0; data->clients[i] != NULL; i++) {
-        if (data->clients[i]->fd != -1 && data->clients[i]->is_registered == 1
-            && data->clients[i]->player == NULL) {
+        if (is_player(data, i) == 0) {
                 dprintf(data->clients[i]->fd, "%s", msg);
         }
     }
 }
 
-static int do_seg(data_t *data, const char *team_name, int is_ctrl_c)
+int do_seg(data_t *data, const char *team_name, const int is_ctrl_c)
 {
     char *msg = NULL;
 
@@ -33,30 +32,5 @@ static int do_seg(data_t *data, const char *team_name, int is_ctrl_c)
     else
         send_to_all_gui(data->clients, msg);
     free(msg);
-    return 0;
-}
-
-static int is_player(data_t *data, int i)
-{
-    if (data->clients[i]->fd != -1 && data->clients[i]->is_registered == 1 &&
-        data->clients[i]->player != NULL &&
-        data->clients[i]->player->team_name != NULL) {
-            return 1;
-    }
-    return 0;
-}
-
-int detect_win(data_t *data, int is_ctrl_c)
-{
-    if (data->clients == NULL)
-        return 1;
-    for (int i = 0; data->clients[i] != NULL; i++) {
-        if (is_ctrl_c == 1 && is_player(data, i) == 1) {
-            do_seg(data, data->clients[i]->player->team_name, 1);
-        }
-        if (is_player(data, i) == 1 && data->clients[i]->player->level == 8) {
-                do_seg(data, data->clients[i]->player->team_name, 0);
-        }
-    }
     return 0;
 }
