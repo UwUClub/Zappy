2aba3767d5cf0e7da6c937b4c365d7ed597231fb
hp <christophe.chhor@epitech.eu>
2023-06-03 16:19:19 +0200

    Fix norm

--- src/server/include/implementation/gui_cmd.h
+++ src/server/include/implementation/gui_cmd.h
@@ -51,11 +51,12 @@
     int do_sgt(data_t *data, char **args);
 
     /**
-     * @brief Send to gui the end of game
+     * @brief Sends to the gui the end of the game
      * @param data The current server data, clients and current client index
-     * @param is_ctrl_c If the end of game is due to a ctrl c
+     * @param team_name The name of the team that won
+     * @param is_ctrl_c If the game ended because of a ctrl-c
      * @return Status of the parsing / command
-     */
-    int detect_win(data_t *data, int is_ctrl_c);
+    */
+    int do_seg(data_t *data, const char *team_name, const int is_ctrl_c);
 
 #endif /* ZAPPY_GUI_CMD_H */
