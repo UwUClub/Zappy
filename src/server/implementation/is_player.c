1255fa91c6edb10c5f7584630c56a8e8e8ee6092
hp <christophe.chhor@epitech.eu>
2023-06-04 18:43:50 +0200

    Fix tabulation on is_player function

--- src/server/implementation/is_player.c
+++ src/server/implementation/is_player.c
@@ -12,7 +12,7 @@ int is_player(data_t *data, const int i)
     if (data->clients[i]->fd != -1 && data->clients[i]->is_registered == 1 &&
         data->clients[i]->player != NULL &&
         data->clients[i]->player->team_name != NULL) {
-            return 1;
+        return 1;
     }
     return 0;
 }
