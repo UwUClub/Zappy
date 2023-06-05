68bf59e7cfe3af6e00945a30f8bf9f6039d96c94
hp <christophe.chhor@epitech.eu>
2023-06-05 13:59:12 +0200

    Add error handling on argument given

--- src/server/implementation/gui/cmd/sst.c
+++ src/server/implementation/gui/cmd/sst.c
@@ -13,11 +13,15 @@ int do_sst(data_t *data, char **args)
 {
     int freq = 0;
 
-    if (!args)
+    if (!args || word_array_len(args) != 1) {
+        send_to_client(data->clients, data->curr_cli_index, "ko\n");
         return 1;
+    }
     freq = atoi(args[0]);
-    if (freq <= 0)
+    if (freq <= 0) {
+        send_to_client(data->clients, data->curr_cli_index, "ko\n");
         return 1;
+    }
     send_to_client(data->clients, data->curr_cli_index, "ok\n");
     data->freq = freq;
     return 0;
