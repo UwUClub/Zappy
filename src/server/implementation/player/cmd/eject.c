098b91414c54c9e995f8b645f88ddc51ad231d3c
Baptiste Laran <baptiste.laran@epitech.eu>
2023-06-06 14:15:11 +0200

    Refacto Eject command

--- src/server/implementation/player/cmd/eject.c
+++ src/server/implementation/player/cmd/eject.c
@@ -13,21 +13,23 @@ static void eject_player(data_t *data, int index)
     player_t *player = data->clients[index]->player;
     char *msg = NULL;
 
-    if (player->pos_x == data->clients[data->curr_cli_index]->player->pos_x &&
-        player->pos_y == data->clients[data->curr_cli_index]->player->pos_y) {
-        if (data->clients[data->curr_cli_index]->player->orientation == NORTH)
-            player->pos_y = (player->pos_y - 1);
-        if (data->clients[data->curr_cli_index]->player->orientation == EAST)
-            player->pos_x = (player->pos_x + 1);
-        if (data->clients[data->curr_cli_index]->player->orientation == SOUTH)
-            player->pos_y = (player->pos_y + 1);
-        if (data->clients[data->curr_cli_index]->player->orientation == WEST)
-            player->pos_x = (player->pos_x - 1);
-        asprintf(&msg, "eject\n", data->clients[data->curr_cli_index]
-                                                ->player->orientation);
-        send_to_client(data->clients, index, msg);
-        free(msg);
+    if (player->pos_x != data->clients[data->curr_cli_index]->player->pos_x ||
+        player->pos_y != data->clients[data->curr_cli_index]->player->pos_y)
+        return;
+    switch (data->clients[data->curr_cli_index]->player->orientation) {
+        case NORTH:
+            player->pos_y -= 1;
+        case SOUTH:
+            player->pos_y += 1;
+        case EAST:
+            player->pos_x += 1;
+        case WEST:
+            player->pos_x -= 1;
     }
+    asprintf(&msg, "eject\n", data->clients[data->curr_cli_index]
+                                            ->player->orientation);
+    send_to_client(data->clients, index, msg);
+    free(msg);
 }
 
 static int eject_cmd(data_t *data, __attribute__((unused)) char **args)
