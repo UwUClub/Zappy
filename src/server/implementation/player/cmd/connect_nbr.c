44dccd54eddbd67521239075a243869ba9afae03
Valentin Gegoux <geg.valentin@gmail.com>
2023-06-06 19:14:06 +0200

    Add Fork + fix Connect_nbr

--- src/server/implementation/player/cmd/connect_nbr.c
+++ src/server/implementation/player/cmd/connect_nbr.c
@@ -16,7 +16,7 @@ static int connect_nbr(data_t *data, __attribute__((unused)) char **args)
 
     nb = get_remaining_slots(data,
     data->clients[data->curr_cli_index]->player->team_name);
-    msg = int_to_s(nb);
+    asprintf(&msg, "%d\n", nb);
     send_to_client(data->clients, data->curr_cli_index, msg);
     free(msg);
     return 0;
--- src/server/implementation/player/cmd/fork.c
+++ src/server/implementation/player/cmd/fork.c
@@ -8,9 +8,37 @@
 #include "implementation.h"
 #include "player_cmd.h"
 
+static void append_egg(data_t *data, const unsigned int team_index,
+    const int pos_x, const int pos_y)
+{
+    int nb_eggs = 0;
+
+    for (int i = 0; data->teams[team_index]->eggs[i]; i++) {
+        nb_eggs++;
+    }
+    data->teams[team_index]->nb_cli++;
+    data->teams[team_index]->eggs = realloc(data->teams[team_index]->eggs,
+        sizeof(pos_t *) * (nb_eggs + 2));
+    data->teams[team_index]->eggs[nb_eggs] = malloc(sizeof(pos_t));
+    data->teams[team_index]->eggs[nb_eggs]->x = pos_x;
+    data->teams[team_index]->eggs[nb_eggs]->y = pos_y;
+    data->teams[team_index]->eggs[nb_eggs + 1] = NULL;
+}
+
 static int fork_cmd(data_t *data, __attribute__((unused)) char **args)
 {
-    return 0;
+    pos_t *player_pos = data->clients[data->curr_cli_index]->player->pos;
+    char *team_name = data->clients[data->curr_cli_index]->player->team_name;
+
+    for (int i = 0; data->teams[i]; i++) {
+        if (!strcmp(team_name, data->teams[i]->name)) {
+            append_egg(data, i, player_pos->x, player_pos->y);
+            send_to_client(data->clients, data->curr_cli_index, "ok\n");
+            return 0;
+        }
+    }
+    send_to_client(data->clients, data->curr_cli_index, "ko\n");
+    return 1;
 }
 
 int schedule_fork(data_t *data, char **args)
