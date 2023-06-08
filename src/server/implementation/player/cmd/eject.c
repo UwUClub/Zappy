76f518d6528c76f93dcb37099b6ead616791907c
Valentin Gegoux <geg.valentin@gmail.com>
2023-06-08 15:23:19 +0200

    Fix eject

--- src/server/implementation/player/cmd/eject.c
+++ src/server/implementation/player/cmd/eject.c
@@ -8,36 +8,50 @@
 #include "implementation.h"
 #include "player_cmd.h"
 
-static void eject_player(data_t *data, int index)
+static void eject_player(data_t *data, const int player_index, player_t *kicker)
 {
-    player_t *player = data->clients[index]->player;
-    char *msg = NULL;
+    player_t *player = data->clients[player_index]->player;
 
-    if (player->pos->x != data->clients[data->curr_cli_index]->player->pos->x
-    || player->pos->y != data->clients[data->curr_cli_index]->player->pos->y)
+    if (player->pos->x != kicker->pos->x || player->pos->y != kicker->pos->y)
         return;
-    switch (data->clients[data->curr_cli_index]->player->orientation) {
+    switch (kicker->orientation) {
         case NORTH:
-            player->pos->y -= 1;
+            data->clients[player_index]->player->pos->y -= 1;
+            break;
         case SOUTH:
-            player->pos->y += 1;
+            data->clients[player_index]->player->pos->y += 1;
+            break;
         case EAST:
-            player->pos->x += 1;
+            data->clients[player_index]->player->pos->x += 1;
+            break;
         case WEST:
-            player->pos->x -= 1;
+            data->clients[player_index]->player->pos->x -= 1;
+            break;
     }
-    asprintf(&msg, "eject\n");
-    send_to_client(data->clients, index, msg);
-    free(msg);
+    normalize_pos(data->clients[player_index]->player->pos, data->map->width,
+        data->map->height);
+    if (player->pending_cmd_queue[0] != NULL) {
+        shift_pending_cmd(data, player_index);
+        send_to_client(data->clients, player_index, "ko\n");
+    }
+    send_to_client(data->clients, player_index, "eject\n");
+    // do pex
+}
+
+static void hatch_eggs_at(data_t *data, const int x, const int y)
+{
 }
 
 static int eject_cmd(data_t *data, __attribute__((unused)) char **args)
 {
+    player_t *kicker = data->clients[data->curr_cli_index]->player;
+
     for (int i = 0; data->clients[i]; i++) {
         if (is_player(data, i) && i != data->curr_cli_index) {
-            eject_player(data, i);
+            eject_player(data, i, kicker);
         }
     }
+    hatch_eggs_at(data, kicker->pos->x, kicker->pos->y);
     send_to_client(data->clients, data->curr_cli_index, "ok\n");
     return 0;
 }
