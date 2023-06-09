313031348af2affe415d74f22042996c40ef3be4
Valentin Gegoux <geg.valentin@gmail.com>
2023-06-09 16:40:59 +0200

    Fix pic concat

--- src/server/implementation/gui/cmd/pic.c
+++ src/server/implementation/gui/cmd/pic.c
@@ -13,14 +13,14 @@ void do_pic(data_t *data, player_t *author)
     char *msg = NULL;
     char *str_id = NULL;
 
-    asprintf(&msg, "pic %d %d %d %d ", author->pos->x, author->pos->y,
+    asprintf(&msg, "pic %d %d %d %d", author->pos->x, author->pos->y,
         author->level, author->id);
     for (int i = 0; data->clients[i]; i++) {
         if (is_player_valid_for_incantation(data->clients, i, author->pos,
         author->level + 1) && data->clients[i]->player->id != author->id) {
             str_id = int_to_s(data->clients[i]->player->id);
-            msg = concat_str(msg, str_id);
             msg = concat_str(msg, " ");
+            msg = concat_str(msg, str_id);
             free(str_id);
         }
     }
