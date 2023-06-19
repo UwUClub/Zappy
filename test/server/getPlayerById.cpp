db67652493d75daf43824fbc2554cd56ff74a9fa
Valentin Gegoux <geg.valentin@gmail.com>
2023-06-19 10:15:54 +0200

    Add tests for get_player_by_id

--- test/server/getPlayerById.cpp
+++ test/server/getPlayerById.cpp
@@ -2,7 +2,7 @@
 #include "../../src/server/implementation/player/get_player_by_id.c"
 #include <catch2/catch_test_macros.hpp>
 
-player_t *init_player(const int id, char *team_name, orientation_t orientation)
+player_t *init_player(const int id, const char *team_name, orientation_t orientation)
 {
     player_t *player = (player_t *) malloc(sizeof(player_t));
 
@@ -18,7 +18,7 @@ player_t *init_player(const int id, char *team_name, orientation_t orientation)
     return player;
 }
 
-client_t *init_player_client(const int id, char *team_name,
+client_t *init_player_client(const int id, const char *team_name,
     orientation_t orientation)
 {
     client_t *client = (client_t *) malloc(sizeof(client_t));
@@ -63,5 +63,53 @@ TEST_CASE("testingGetPlayerById1", "[testingGetPlayerById1]")
     REQUIRE(player->id == 42);
     REQUIRE(!strcmp(player->team_name, "Team2"));
     REQUIRE(player->orientation == EAST);
+    free_clients(clients);
+}
+
+TEST_CASE("testingGetPlayerById2", "[testingGetPlayerById2]")
+{
+    client_t **clients = (client_t **) malloc(sizeof(client_t *) * 4);
+    player_t *player = NULL;
+
+    clients[0] = init_player_client(15, "Team1", NORTH);
+    clients[1] = init_player_client(42, "Team2", EAST);
+    clients[2] = init_player_client(86, "Team3", SOUTH);
+    clients[3] = NULL;
+    player = get_player_by_id(clients, 15);
+    REQUIRE(player != NULL);
+    REQUIRE(player->id == 15);
+    REQUIRE(!strcmp(player->team_name, "Team1"));
+    REQUIRE(player->orientation == NORTH);
+    free_clients(clients);
+}
+
+TEST_CASE("testingGetPlayerById3", "[testingGetPlayerById3]")
+{
+    client_t **clients = (client_t **) malloc(sizeof(client_t *) * 4);
+    player_t *player = NULL;
+
+    clients[0] = init_player_client(15, "Team1", NORTH);
+    clients[1] = init_player_client(42, "Team2", EAST);
+    clients[2] = init_player_client(86, "Team3", SOUTH);
+    clients[3] = NULL;
+    player = get_player_by_id(clients, 86);
+    REQUIRE(player != NULL);
+    REQUIRE(player->id == 86);
+    REQUIRE(!strcmp(player->team_name, "Team3"));
+    REQUIRE(player->orientation == SOUTH);
+    free_clients(clients);
+}
+
+TEST_CASE("testingGetPlayerById4", "[testingGetPlayerById4]")
+{
+    client_t **clients = (client_t **) malloc(sizeof(client_t *) * 4);
+    player_t *player = NULL;
+
+    clients[0] = init_player_client(15, "Team1", NORTH);
+    clients[1] = init_player_client(42, "Team2", EAST);
+    clients[2] = init_player_client(86, "Team3", SOUTH);
+    clients[3] = NULL;
+    player = get_player_by_id(clients, 0);
+    REQUIRE(player == NULL);
     free_clients(clients);
 }
