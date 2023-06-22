22444f6fac75a49ae6000aa710645893b260e005
Matheo <matheo.martin@epitech.eu>
2023-06-22 13:55:29 +0200

    Fix tests

--- test/gui/testCommandGuiCli.cpp
+++ test/gui/testCommandGuiCli.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-#include "../../src/gui/cli/ClientApi.hpp"
-#include "../../src/gui/cli/ParserData.hpp"
+#include "ClientApi.hpp"
+#include "ParserData.hpp"
+#include "App.hpp"
+#include "Mediator.hpp"
+#include "ServerData.hpp"
 #include <catch2/catch_test_macros.hpp>
 
 constexpr auto SERVER_IP = "127.0.0.1";
@@ -8,14 +10,17 @@ constexpr auto SERVER_PORT = 4242;
 
 TEST_CASE("testingBct", "[testingBct]")
 {
-    Zappy::GUI::ParserData const parserData(SERVER_IP, SERVER_PORT, "");
-    Zappy::GUI::ClientApi myClientApi(parserData.getAddress(), parserData.getPort(), "GRAPHIC");
+    Zappy::GUI::ParserData parserData(SERVER_IP, SERVER_PORT, "");
+    Zappy::GUI::ServerData myServerData;
+    Zappy::GUI::Mediator myMediator(parserData);
+    Zappy::GUI::ClientApi myClientApi("127.0.0.1", 4242, "GRAPHIC", myMediator, myServerData);
+
     myClientApi.joinGame();
     while (true) {
         if (myClientApi.update() >= 1) {
             break;
         }
-        if (!myClientApi.getServerData()._mapTiles.empty()) {
+        if (myClientApi.getServerData()._mapSize.first) {
             break;
         }
     }
@@ -25,8 +30,11 @@ TEST_CASE("testingBct", "[testingBct]")
 
 TEST_CASE("testingMsz", "[testingMsz]")
 {
-    Zappy::GUI::ParserData const parserData(SERVER_IP, SERVER_PORT, "");
-    Zappy::GUI::ClientApi myClientApi(parserData.getAddress(), parserData.getPort(), "GRAPHIC");
+    Zappy::GUI::ParserData parserData(SERVER_IP, SERVER_PORT, "");
+    Zappy::GUI::ServerData myServerData;
+    Zappy::GUI::Mediator myMediator(parserData);
+    Zappy::GUI::ClientApi myClientApi("127.0.0.1", 4242, "GRAPHIC", myMediator, myServerData);
+
     myClientApi.joinGame();
     while (true) {
         if (myClientApi.update() >= 1) {
@@ -43,8 +51,11 @@ TEST_CASE("testingMsz", "[testingMsz]")
 
 TEST_CASE("testingTna", "[testingTna]")
 {
-    Zappy::GUI::ParserData const parserData(SERVER_IP, SERVER_PORT, "");
-    Zappy::GUI::ClientApi myClientApi(parserData.getAddress(), parserData.getPort(), "GRAPHIC");
+    Zappy::GUI::ParserData parserData(SERVER_IP, SERVER_PORT, "");
+    Zappy::GUI::ServerData myServerData;
+    Zappy::GUI::Mediator myMediator(parserData);
+    Zappy::GUI::ClientApi myClientApi("127.0.0.1", 4242, "GRAPHIC", myMediator, myServerData);
+
     myClientApi.joinGame();
     while (true) {
         if (myClientApi.update() >= 1) {
@@ -60,8 +71,11 @@ TEST_CASE("testingTna", "[testingTna]")
 
 TEST_CASE("testingPpo", "[testingPpo]")
 {
-    Zappy::GUI::ParserData const parserData(SERVER_IP, SERVER_PORT, "");
-    Zappy::GUI::ClientApi myClientApi(parserData.getAddress(), parserData.getPort(), "GRAPHIC");
+    Zappy::GUI::ParserData parserData(SERVER_IP, SERVER_PORT, "");
+    Zappy::GUI::ServerData myServerData;
+    Zappy::GUI::Mediator myMediator(parserData);
+    Zappy::GUI::ClientApi myClientApi("127.0.0.1", 4242, "GRAPHIC", myMediator, myServerData);
+
     myClientApi.joinGame();
     while (true) {
         if (myClientApi.update() >= 1) {
@@ -85,8 +99,11 @@ TEST_CASE("testingPpo", "[testingPpo]")
 
 TEST_CASE("testingPlv", "[testingPlv]")
 {
-    Zappy::GUI::ParserData const parserData(SERVER_IP, SERVER_PORT, "");
-    Zappy::GUI::ClientApi myClientApi(parserData.getAddress(), parserData.getPort(), "GRAPHIC");
+    Zappy::GUI::ParserData parserData(SERVER_IP, SERVER_PORT, "");
+    Zappy::GUI::ServerData myServerData;
+    Zappy::GUI::Mediator myMediator(parserData);
+    Zappy::GUI::ClientApi myClientApi("127.0.0.1", 4242, "GRAPHIC", myMediator, myServerData);
+
     myClientApi.joinGame();
     while (true) {
         if (myClientApi.update() >= 1) {
@@ -105,4 +122,44 @@ TEST_CASE("testingPlv", "[testingPlv]")
     }
     myClientApi.disconnect();
     REQUIRE(myClientApi.getServerData()._players.at(0).getLevel() != 0);
+}
+
+TEST_CASE("testingEnw", "[testingEnw]")
+{
+    Zappy::GUI::ParserData parserData(SERVER_IP, SERVER_PORT, "");
+    Zappy::GUI::ServerData myServerData;
+    Zappy::GUI::Mediator myMediator(parserData);
+    Zappy::GUI::ClientApi myClientApi("127.0.0.1", 4242, "GRAPHIC", myMediator, myServerData);
+
+    myClientApi.joinGame();
+    while (true) {
+        if (myClientApi.update() >= 1) {
+            break;
+        }
+        if (!myClientApi.getServerData()._eggs.empty()) {
+            break;
+        }
+    }
+    myClientApi.disconnect();
+    REQUIRE(!myClientApi.getServerData()._eggs.empty());
+}
+
+TEST_CASE("testingSgt", "[testingSgt]")
+{
+    Zappy::GUI::ParserData parserData(SERVER_IP, SERVER_PORT, "");
+    Zappy::GUI::ServerData myServerData;
+    Zappy::GUI::Mediator myMediator(parserData);
+    Zappy::GUI::ClientApi myClientApi("127.0.0.1", 4242, "GRAPHIC", myMediator, myServerData);
+
+    myClientApi.joinGame();
+    while (true) {
+        if (myClientApi.update() >= 1) {
+            break;
+        }
+        if (myClientApi.getServerData()._freq != 0) {
+            break;
+        }
+    }
+    myClientApi.disconnect();
+    REQUIRE(myClientApi.getServerData()._freq != 0);
 }
